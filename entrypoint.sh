07277c2586f08494817ce9a0c19a57598c6d9804
renedis <renedis@hotmail.com>
2026-02-24 14:58:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,14 @@ set -e
 mkdir -p /tmp/powertop
 
 while true; do
-    powertop --html=/tmp/powertop/report.html --time=20 2>/dev/null || true
+    TMP="/tmp/powertop/report.tmp.html"
+
+    powertop --html="$TMP" --time=20 2>/dev/null || true
+
+    if [ -s "$TMP" ]; then
+        mv "$TMP" /tmp/powertop/report.html
+    fi
+
     sleep 40
 done &
 
